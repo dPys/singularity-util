d22d0e21515f78c99b6f361d7ac270dfa7b06ce4
dpys <dpisner@utexas.edu>
2021-07-07 02:56:46 -0500

    next commit

--- skel/build.sh
+++ skel/build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+export SINGULARITY_VERSION="2.6.1"
 cd build || exit 1
-git clone https://github.com/singularityware/singularity.git
+git clone https://github.com/sylabs/singularity.git
 cd singularity || exit 1
 git checkout "$SINGULARITY_VERSION"
+git fetch --all
 ./autogen.sh
 ./configure
 make
