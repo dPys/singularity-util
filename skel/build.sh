37e93f2a153f653f34f995c383f70d6dd563c1c8
dpys <dpisner@utexas.edu>
2021-07-07 20:43:22 -0500

    update

--- skel/build.sh
+++ skel/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-export SINGULARITY_VERSION="3.7.2"
+export SINGULARITY_VERSION="3.6.4"
 cd build || exit 1
 git clone https://github.com/sylabs/singularity.git
 cd singularity || exit 1
 git checkout "$SINGULARITY_VERSION"
 git fetch --all
 ./mconfig
-make -C builddir
-sudo make -C builddir install
+make -C ./builddir
+sudo make -C ./builddir install
